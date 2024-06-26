ad7f6707f2c2e046d5a67346b40676cd2c7185f3
Frank Suarez <fsuarez2007@gmail.com>
2024-06-26 11:56:47 -0400

    string permutation function

---
+++ java/app/src/main/java/com/franksuarez/dsa/Permutation.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.franksuarez.dsa;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author franksuarez
+ */
+public class Permutation {
+    
+    /** Returns permutations of a String.
+     * 
+     * @param s 
+     */
+    public static void permutation(String s) {
+        System.out.printf("s = %s%n",s);
+        // s.length == 2
+        // split string into char array
+        List<String> asList = Arrays.asList(s.split(""));
+        // take first char and place second char in all places
+        
+        
+        
+        
+        
+    }
+}
---
+++ java/app/src/test/java/com/franksuarez/dsa/PermutationTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.franksuarez.dsa;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author franksuarez
+ */
+public class PermutationTest {
+    
+    public PermutationTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of permutation method, of class Permutation.
+     */
+    @Test
+    public void testPermutation() {
+        System.out.println("permutation");
+        String s = "ab";
+        Permutation.permutation(s);
+    }
+    
+}
